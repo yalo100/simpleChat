ed56635c8e765e2b6ab57e5e7eddc3ba86a8c6de
yalo100 <82458699+yalo100@users.noreply.github.com>
2022-11-07 00:22:48 -0500

    New commit

--- client/ChatClient.java
+++ client/ChatClient.java
@@ -90,5 +90,28 @@ public class ChatClient extends AbstractClient
     catch(IOException e) {}
     System.exit(0);
   }
+  /**
+	 * Hook method called after the connection has been closed. The default
+	 * implementation does nothing. The method may be overriden by subclasses to
+	 * perform special processing such as cleaning up and terminating, or
+	 * attempting to reconnect.
+	 */
+    @Override	
+	protected void connectionClosed() {
+		
+	}
+
+	/**
+	 * Hook method called each time an exception is thrown by the client's
+	 * thread that is waiting for messages from the server. The method may be
+	 * overridden by subclasses.
+	 * 
+	 * @param exception
+	 *            the exception raised.
+	 */
+	@Override
+	protected void connectionException(Exception exception) {
+	 clientUI.display("The server has shut down");
+	}
 }
 //End of ChatClient class
